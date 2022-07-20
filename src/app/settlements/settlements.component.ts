6eecb0a76d1bce842c93e043fb3063a5e9e0bc39
Jason De Lorme <jasondel@google.com>
2022-07-20 22:47:29 +0000

    Initial commit

---
+++ src/app/settlements/settlements.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'settlements',
+  templateUrl: './settlements.component.html',
+  styleUrls: ['./settlements.component.css']
+})
+export class SettlementsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
