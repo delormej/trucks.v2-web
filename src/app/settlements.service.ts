028f7c1c5e27c1cd01a948bff12f3ff1be1e6717
Jason De Lorme <delormej@gmail.com>
2022-08-17 11:55:10 -0400

    Using env for service url

--- src/app/settlements.service.ts
+++ src/app/settlements.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SettlementsService {
 
-  public static readonly baseUrl: string =  "http://localhost:5000/api"; //  "https://trucks.jasondel.com/api"; //
+  public static readonly baseUrl: string = environment.apiUrl; 
 
   constructor(private http: HttpClient) { }
 
