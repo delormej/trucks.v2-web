d271232c56a6f42dbd4940ef46d1ad4f5cf91b44
Jason De Lorme <delormej@gmail.com>
2022-10-11 06:30:52 -0400

    Paginating table added.

--- src/app/settlements/settlements.component.ts
+++ src/app/settlements/settlements.component.ts
@@ -1,25 +1,61 @@
-import { Component, OnInit } from '@angular/core';
-import { SettlementsService, Summary } from '../settlements.service';
-import { Observable, of } from 'rxjs';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { SettlementsService, Summary } from '../settlements.service'; 
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ProgressSpinnerMode } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'settlements',
   templateUrl: './settlements.component.html',
   styleUrls: ['./settlements.component.css']
 })
-export class SettlementsComponent implements OnInit {
+export class SettlementsComponent implements OnInit, AfterViewInit {
+  loadingProgressMode!: ProgressSpinnerMode;
+  loading: boolean = false;
+  dataSource = new MatTableDataSource<Summary>();
+  displayedColumns: string[] = [
+    "companyId",
+    "settlementId",
+    "yearWeek",
+    "settlementDate",
+    "checkAmount"
+  ];
 
-  summaries: Summary[] = [];
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
-  constructor(private settlementsService: SettlementsService ) { }
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
 
   ngOnInit(): void {
     this.getSummaries();
   }
 
+  constructor(
+    private settlementsService: SettlementsService,
+    private snack: MatSnackBar) { }
+
   getSummaries(): void {
-    this.settlementsService.getSettlementSummaries().subscribe(res => {
-      this.summaries = res;
+    this.loading = true;
+    this.loadingProgressMode = "indeterminate";
+    this.settlementsService.getSettlementSummaries()
+    .subscribe({
+      next: (data) => {
+        this.dataSource.data = data;
+        this.loadingProgressMode = "determinate"; 
+        this.loading = false;
+      },
+      error: (error) => {
+        this.showError(error, "Unable to load settlements.");
+        this.loadingProgressMode = "determinate";
+        this.loading = false;
+      }
     });
   }
+
+  showError(error: Error, message: string) {
+    this.snack.open(message, 'CLOSE', { panelClass: 'errorSnack' } );
+    console.log(error);
+  }  
 }
