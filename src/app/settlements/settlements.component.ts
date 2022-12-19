3a4f3b66629374b1ec246096fb0893ab01b0755a
Jason De Lorme <delormej@gmail.com>
2022-12-19 06:42:06 -0500

    Cleanup

--- src/app/settlements/settlements.component.ts
+++ src/app/settlements/settlements.component.ts
@@ -2,7 +2,6 @@ import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { ProgressSpinnerMode } from '@angular/material/progress-spinner';
 import { SettlementsService } from '../settlements.service'; 
 import { SettlementSummary } from '../settlements.service.types';
 
